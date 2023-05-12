e3346f8243f189aeb9ee43fa95e4f3814ccb520f
Nicolas Raj <me@nicolasraj.com>
2023-05-12 10:16:44 +0800

    updated xlsx writer

--- Shopify.py
+++ Shopify.py
@@ -47,7 +47,7 @@ def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, sheet_name='Sheet1', index=False)
-    writer.save()
+    writer.close()
     processed_data = output.getvalue()
     return processed_data
 
